b68e1e7117c8aa9f7e73c8d8c67e58c3cc5eb107
Холодильник <galina.777fotostock@gmail.com>
2025-12-19 20:17:43 +0200

    Update nathaniel.py

--- nathaniel.py
+++ nathaniel.py
@@ -21,11 +21,31 @@ def board_draw(canvas):
     for i in range(1, 3):
         canvas.create_line(i*cell, 0, i*cell, size)
 
-board_draw(canvas) #як визвати функцию та намалювати поле
+# board_draw(canvas) #як визвати функцию та намалювати поле
 # зараз не працюе тому що нема коду що створюе холст, ним вроде як займатся хтось iнший
 # код пример з яким вiн може запрацювати (такий код ставиться у самому верху):
 def board_clear(canvas):
-    # Очищаемо холст вiD ходiB
     canvas.delete("all")
+    for i in range(3):
+        for j in range(3):
+            board[0][i][j] = ""
     board_draw(canvas)
 
+def on_click(event):
+    global current_player
+
+    size = int(canvas["width"])
+    cell = size // 3
+    col = event.x // cell
+    row = event.y // cell
+
+    if row > 2 or col > 2:
+        return
+    if board[0][row][col] == "":
+        board[0][row][col] = current_player
+        draw_symbol(row, col, current_player)
+        current_player = "O" if current_player == "X" else "X"
+
+board_draw(canvas)
+canvas.bind("<Button-1>", on_click)
+root.mainloop()
